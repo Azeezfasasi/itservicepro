4a2148b71b01e5f35271d960db1280ef403a1004
azeezfasasi <fas.azeez@gmail.com>
2025-06-26 12:54:14 +0100

    updated menu icons

--- src/assets/components/dashboard-components/DashMenu.jsx
+++ src/assets/components/dashboard-components/DashMenu.jsx
@@ -1,8 +1,14 @@
 import { Sidenav, Nav } from 'rsuite';
 import { Link, useLocation } from 'react-router-dom';
 import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GroupIcon from '@rsuite/icons/legacy/Group';
-import MagicIcon from '@rsuite/icons/legacy/Magic';
+import DetailIcon from '@rsuite/icons/Detail';
+import ListIcon from '@rsuite/icons/List';
+import UserInfoIcon from '@rsuite/icons/UserInfo';
+import PeoplesIcon from '@rsuite/icons/Peoples';
+import GridIcon from '@rsuite/icons/Grid';
+import TagIcon from '@rsuite/icons/Tag';
+import MessageIcon from '@rsuite/icons/Message';
+import GearIcon from '@rsuite/icons/Gear';
 import { useUser } from '../../context-api/user-context/UseUser';
 
 function DashMenu() {
@@ -44,30 +50,30 @@ function DashMenu() {
                     </Nav.Item>
                     )}
                     {(isSuperAdmin || isAdmin) && (
-                    <Nav.Item eventKey="2" icon={<GroupIcon />} as={Link} to="/app/quote">
+                    <Nav.Item eventKey="2" icon={<DetailIcon />} as={Link} to="/app/quote">
                         Quote Requests
                     </Nav.Item>
                     )}
                     {(isSuperAdmin || isAdmin) && (
-                    <Nav.Menu eventKey="3" title="Blog Post" icon={<MagicIcon />}>
+                    <Nav.Menu eventKey="3" title="Blog Post" icon={<ListIcon />}>
                         <Nav.Item eventKey="3-1" as={Link} to="/app/blogposts">All Posts</Nav.Item>
                         <Nav.Item eventKey="3-2" as={Link} to="/app/addnewpost">Add New Post</Nav.Item>
                     </Nav.Menu>
                     )}
                     {(isSuperAdmin || isAdmin || isUser || isCustomer) && (
-                    <Nav.Item eventKey="4" icon={<GroupIcon />} as={Link} to="/app/profile">
+                    <Nav.Item eventKey="4" icon={<UserInfoIcon />} as={Link} to="/app/profile">
                         Profile
                     </Nav.Item>
                     )}
                     {(isSuperAdmin || isAdmin) && (
-                    <Nav.Menu eventKey="5" title="Users" icon={<MagicIcon />}>
+                    <Nav.Menu eventKey="5" title="Users" icon={<PeoplesIcon />}>
                         <Nav.Item eventKey="5-1" as={Link} to="/app/allusers">All Users</Nav.Item>
                         <Nav.Item eventKey="5-2" as={Link} to="/app/addnewuser">Add New User</Nav.Item>
                         <Nav.Item eventKey="5-2" as={Link} to="/app/changeuserpassword">Change User Password</Nav.Item>
                     </Nav.Menu>
                     )}
                     {(isSuperAdmin || isAdmin) && (
-                    <Nav.Menu eventKey="6" title="Product" icon={<MagicIcon />}>
+                    <Nav.Menu eventKey="6" title="Product" icon={<GridIcon />}>
                         <Nav.Item eventKey="6-1" as={Link} to="/app/products">All Products</Nav.Item>
                         <Nav.Item eventKey="5-2" as={Link} to="/app/addproduct">Add Products</Nav.Item>
                         <Nav.Item eventKey="5-2" as={Link} to="/app/productcategories">Product Categories</Nav.Item>
@@ -75,24 +81,24 @@ function DashMenu() {
                     </Nav.Menu>
                     )}
                     {(isUser || isCustomer) && (
-                    <Nav.Item eventKey="7" icon={<GroupIcon />} as={Link} to="/app/userorderdetails">
+                    <Nav.Item eventKey="7" icon={<TagIcon />} as={Link} to="/app/userorderdetails">
                         My Order
                     </Nav.Item>
                     )}
                     {(isSuperAdmin || isAdmin) && (
-                    <Nav.Item eventKey="8" icon={<GroupIcon />} as={Link} to="/app/adminorderlist">
+                    <Nav.Item eventKey="8" icon={<TagIcon />} as={Link} to="/app/adminorderlist">
                         All Order 
                     </Nav.Item>
                     )}
                     {(isSuperAdmin || isAdmin) && (
-                    <Nav.Menu eventKey="9" title="Newsletter" icon={<MagicIcon />}>
+                    <Nav.Menu eventKey="9" title="Newsletter" icon={<MessageIcon />}>
                         <Nav.Item eventKey="9-1" as={Link} to="/app/sendnewsletter">Send Newsletter</Nav.Item>
                         <Nav.Item eventKey="9-2" as={Link} to="/app/allnewsletter">All Newsletters</Nav.Item>
                         <Nav.Item eventKey="9-3" as={Link} to="/app/Newslettersubscribers">Subscribers</Nav.Item>
                     </Nav.Menu>
                     )}
                     {(isSuperAdmin || isAdmin || isUser || isCustomer) && (
-                    <Nav.Item eventKey="10" icon={<GroupIcon />} as={Link} to="/app/mysettings">
+                    <Nav.Item eventKey="10" icon={<GearIcon />} as={Link} to="/app/mysettings">
                         Settings
                     </Nav.Item>
                     )}
