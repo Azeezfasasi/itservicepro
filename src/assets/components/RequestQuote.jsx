533f7c4fb6331f7395eae4334bd22df539cd75ca
azeezfasasi <fas.azeez@gmail.com>
2025-07-16 07:37:37 +0100

    updated email

--- src/assets/components/RequestQuote.jsx
+++ src/assets/components/RequestQuote.jsx
@@ -49,7 +49,7 @@ function RequestQuote() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-phone text-blue-500">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.63A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
                 </svg>
-                <span className="text-gray-700">Fast Service: <a href="tel:202-555-0187" className="text-blue-500 hover:underline">(+234) 0811725648</a></span>
+                <span className="text-gray-700">Fast Service: <a href="tel:08103069432" className="text-blue-500 hover:underline">(+234) 08103069432</a></span>
               </div>
               <div className="flex items-center space-x-3">
                 {/* Mail icon */}
@@ -57,7 +57,7 @@ function RequestQuote() {
                   <rect width="20" height="16" x="2" y="4" rx="2"/>
                   <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/>
                 </svg>
-                <span className="text-gray-700"><a href="mailto:infohomepro@email.com" className="text-blue-500 hover:underline">info@itservicepro.com</a></span>
+                <span className="text-gray-700"><a href="mailto:info@mgv-tech.com" className="text-blue-500 hover:underline">info@mgv-tech.com</a></span>
               </div>
             </div>
           </div>
